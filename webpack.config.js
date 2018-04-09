2bc58d33d94080e242f667a8c5ba7c0b690b1c17
Alex Zi <VikingRock@users.noreply.github.com>
2018-04-09 17:16:46 +0300

    using Webpack for copying

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,12 @@
 'use strict';
 
 var path = require('path');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+var JSAssets = [
+  'jquery/dist/jquery.min.js',
+  'lightslider/dist/js/lightslider.min.js'
+];
 
 module.exports = {
   devServer: {
@@ -14,6 +20,22 @@ module.exports = {
     path: path.resolve(__dirname),
     sourceMapFilename: '[file].map'
   },
+
+  plugins: [
+    new CopyWebpackPlugin(
+      JSAssets.map(function(asset) {
+        return {
+          from: path.resolve(__dirname, './node_modules/' + asset),
+          to: path.resolve(__dirname, './libs')
+        };
+      })
+    ),
+    new CopyWebpackPlugin([
+      {
+        from: path.resolve(__dirname, './node_modules/lightslider/dist/css/lightslider.min.css'),
+        to: path.resolve(__dirname, '../css/libs') }])
+  ],
+
 
   resolve: {
     modulesDirectories: ['node_modules', './source/js', './lib', './vendor']
